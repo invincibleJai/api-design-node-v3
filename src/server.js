3ed962b1d78ad6a72e3c7596ecf61ffaa025ab99
Jaivardhan Kumar <mailjai.vardhan@gmail.com>
2020-10-14 10:11:47 +0530

    lesson 1 soln

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ import morgan from 'morgan'
 import cors from 'cors'
 
 export const app = express()
+const router = express.Router()
 
 app.disable('x-powered-by')
 
@@ -12,4 +13,35 @@ app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(morgan('dev'))
 
-export const start = () => {}
+// Custom middleware
+const log = (req, res, next) => {
+  console.log('logging')
+  next()
+}
+
+app.use(log)
+// Needed to add router
+app.use('/api', router)
+
+app.get('/', (req, res) => {
+  res.send('hello world12')
+})
+
+router.get('/route', (req, res) => {
+  res.send('am in router')
+})
+
+app.get('/data', (req, res) => {
+  console.log('am in')
+  res.send({ message: 'hello world' })
+})
+
+app.post('/data', (req, res) => {
+  res.send(req.body)
+})
+
+export const start = () => {
+  app.listen(3000, () => {
+    console.log('Server running on 3000')
+  })
+}
